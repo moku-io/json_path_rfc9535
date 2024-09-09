aac4dcb0c811cceccdc736099a9dda4956d76a44
Riccardo Agatea <riccardo@moku.io>
2024-09-09 14:42:29 +0200

    add methods to query JSON document

--- lib/json_path/doc.rb
+++ lib/json_path/doc.rb
@@ -1,5 +1,7 @@
 require 'json'
 require_relative 'nodes'
+require_relative 'path'
+require_relative 'node_list'
 
 module JsonPath
   class Doc
@@ -7,6 +9,18 @@ module JsonPath
 
     def initialize json_string
       @root_node = Nodes.parse '$', JSON.parse(json_string)
+    end
+
+    def query json_path
+      json_path = Path.new json_path
+
+      json_path
+        .apply(root_node)
+        .then { NodeList.new _1 }
+    end
+
+    def value
+      root_node.value
     end
   end
 end
